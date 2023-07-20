bb10a5e61fbc63b14d51fe2de6b76cc7068a80c8
ziyang <791191656@qq.com>
2023-07-20 01:57:17 -0700

    modified zdtree

--- benchmarks/nearestNeighbors/bench/neighborsTime.C
+++ benchmarks/nearestNeighbors/bench/neighborsTime.C
@@ -20,14 +20,15 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include <iostream>
 #include <algorithm>
-#include "parlay/parallel.h"
-#include "parlay/primitives.h"
+#include <iostream>
+
 #include "common/geometry.h"
 #include "common/geometryIO.h"
 #include "common/parse_command_line.h"
 #include "common/time_loop.h"
+#include "parlay/parallel.h"
+#include "parlay/primitives.h"
 using namespace benchIO;
 
 // *************************************************************
@@ -39,14 +40,15 @@ using point2 = point2d<coord>;
 using point3 = point3d<coord>;
 
 template <class PT, int KK>
-struct vertex {
-  using pointT = PT;
-  int identifier;
-  pointT pt;         // the point itself
-  vertex* ngh[KK];    // the list of neighbors
-  vertex(pointT p, int id) : pt(p), identifier(id) {}
-  size_t counter;
-  size_t counter2; 
+struct vertex
+{
+   using pointT = PT;
+   int identifier;
+   pointT pt;        // the point itself
+   vertex* ngh[KK];  // the list of neighbors
+   vertex( pointT p, int id ) : pt( p ), identifier( id ) {}
+   size_t counter;
+   size_t counter2;
 };
 
 // *************************************************************
@@ -54,56 +56,82 @@ struct vertex {
 // *************************************************************
 
 template <int maxK, class point>
-void timeNeighbors(parlay::sequence<point> &pts, int k, int rounds, char* outFile) {
-  size_t n = pts.size();
-  using vtx = vertex<point,maxK>;
-  int dimensions = pts[0].dimension();
-  auto vv = parlay::tabulate(n, [&] (size_t i) -> vtx {
-      return vtx(pts[i],i);
-    });
-  auto v = parlay::tabulate(n, [&] (size_t i) -> vtx* {
-      return &vv[i];});
+void
+timeNeighbors( parlay::sequence<point>& pts, int k, int rounds, char* outFile )
+{
+   size_t n = pts.size();
+   using vtx = vertex<point, maxK>;
+   int dimensions = pts[0].dimension();
+   auto vv = parlay::tabulate(
+       n, [&]( size_t i ) -> vtx { return vtx( pts[i], i ); } );
+   auto v = parlay::tabulate( n, [&]( size_t i ) -> vtx* { return &vv[i]; } );
 
-  // run once for warmup
-  time_loop(rounds, 1.0,
-	    [&] () {},
-	    [&] () {ANN<maxK>(v, k);},
-	    [&] () {});
-
-  if (outFile != NULL) {
-    int m = n * k;
-    parlay::sequence<int> Pout(m);
-    parlay::parallel_for (0, n-1, [&] (size_t i) {
-
-	for (int j=0; j < k; j++){
-	  Pout[k*i + j] = (v[i]->ngh[j])->identifier;
-  }
-      });
-    writeIntSeqToFile(Pout, outFile);
-  }
+   ANN<maxK>( v, k );
+   if( outFile != NULL )
+   {
+      int m = n * k;
+      parlay::sequence<int> Pout( m );
+      parlay::parallel_for( 0, n - 1,
+                            [&]( size_t i )
+                            {
+                               for( int j = 0; j < k; j++ )
+                               {
+                                  Pout[k * i + j] =
+                                      ( v[i]->ngh[j] )->identifier;
+                               }
+                            } );
+      writeIntSeqToFile( Pout, outFile );
+   }
 }
 
-int main(int argc, char* argv[]) {
-  commandLine P(argc,argv,"[-k {1,...,100}] [-d {2,3}] [-o <outFile>] [-r <rounds>] <inFile>");
-  char* iFile = P.getArgument(0);
-  char* oFile = P.getOptionValue("-o");
-  int rounds = P.getOptionIntValue("-r",1);
-  int k = P.getOptionIntValue("-k",1);
-  int d = P.getOptionIntValue("-d",2);
-  algorithm_version = P.getOptionIntValue("-t",algorithm_version);
-  if (k < 1 || k>100) P.badArgument();
-  if (d < 2 || d > 3) P.badArgument();
+template <class Point>
+parlay::sequence<Point>
+readGeneral( char const* fname )
+{
+   parlay::sequence<char> S = readStringFromFile( fname );
+   parlay::sequence<char*> W = stringToWords( S );
+   int d = Point::dim;
+   //  std::cout << W.size() << std::endl;
+   return parsePoints<Point>( W.cut( 2, W.size() ) );
+}
 
-  if (d == 2) {
-    parlay::sequence<point2> PIn = readPointsFromFile<point2>(iFile);
-    if (k == 1) timeNeighbors<1>(PIn, 1, rounds, oFile);
-    else timeNeighbors<100>(PIn, k, rounds, oFile);
-  }
+int
+main( int argc, char* argv[] )
+{
+   commandLine P( argc, argv,
+                  "[-k {1,...,100}] [-d {2,3}] [-o <outFile>] [-r <rounds>] "
+                  "[-p <inFile>] [-t <tag>]" );
+   char* iFile = P.getOptionValue( "-p" );
+   char* oFile = P.getOptionValue( "-o" );
+   int rounds = P.getOptionIntValue( "-r", 1 );
+   int k = P.getOptionIntValue( "-k", 1 );
+   int d = P.getOptionIntValue( "-d", 3 );
+   int tag = P.getOptionIntValue( "-t", -1 );
+   //  algorithm_version = P.getOptionIntValue( "-t", algorithm_version );
+   if( k < 1 || k > 100 ) P.badArgument();
+   if( d < 2 || d > 3 ) P.badArgument();
 
-  if (d == 3) {
-    parlay::sequence<point3> PIn = readPointsFromFile<point3>(iFile);
-    if (k == 1) timeNeighbors<1>(PIn, 1, rounds, oFile);
-    else timeNeighbors<100>(PIn, k, rounds, oFile);
-  }
+   std::string name( iFile );
+   name = name.substr( name.rfind( "/" ) + 1 );
+   std::cout << name << " ";
 
+   if( d == 2 )
+   {
+      // parlay::sequence<point2> PIn = readPointsFromFile<point2>( iFile );
+      parlay::sequence<point2> PIn = readGeneral<point2>( iFile );
+      if( k == 1 )
+         timeNeighbors<1>( PIn, 1, rounds, oFile );
+      else
+         timeNeighbors<100>( PIn, k, rounds, oFile );
+   }
+
+   if( d == 3 )
+   {
+      // parlay::sequence<point3> PIn = readPointsFromFile<point3>( iFile );
+      parlay::sequence<point3> PIn = readGeneral<point3>( iFile );
+      if( k == 1 )
+         timeNeighbors<1>( PIn, 1, rounds, oFile );
+      else
+         timeNeighbors<100>( PIn, k, rounds, oFile );
+   }
 }
